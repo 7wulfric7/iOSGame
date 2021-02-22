ed793485fa3848e0291c684079f204f8ab055155
Deniz Adil <xdenizadil@gmail.com>
2021-02-22 14:42:52 +0100

    • Updated UI to the tableViewCell, using user's avatar image, play button, and uppercased username.

--- iOSGame/iOSGame/Cells/UserTableViewCell.swift
+++ iOSGame/iOSGame/Cells/UserTableViewCell.swift
@@ -11,6 +11,7 @@ import SnapKit
 protocol UserCellDelegate: class {
     func requestGameWith(user: User)
 }
+
 class UserTableViewCell: UITableViewCell {
 // Storyboard:
 //    @IBOutlet weak var lblUserName: UILabel!
@@ -21,20 +22,36 @@ class UserTableViewCell: UITableViewCell {
         activity.color = .red
         return activity
     }()
+    
+    private lazy var backgroundImage: UIView = {
+        let imageView = UIView()
+        imageView.layer.backgroundColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 20
+        return imageView
+    }()
+    
+    private lazy var userImage: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
     lazy var lblUsername: UILabel = {
-       var label = UILabel()
-        label.textColor = UIColor(named: "SystemOposite")
+        var label = UILabel()
+        label.textColor = UIColor(hex: "4A6495")
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         return label
     }()
+    
     private lazy var btnStart: UIButton = {
         var button = UIButton()
-        button.setTitle("Start Game", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
-        button.setTitleColor(UIColor(named: "SystemOposite"), for: .normal)
-        button.layer.borderWidth = 1.0
-        button.layer.borderColor = UIColor.red.cgColor
-        button.layer.cornerRadius = 5
-        button.layer.masksToBounds = true
+        button.setImage(UIImage(named: "StartGameButton"), for: .normal)
+//        button.setTitle("Start Game", for: .normal)
+//        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+//        button.setTitleColor(UIColor(named: "SystemOposite"), for: .normal)
+//        button.layer.borderWidth = 1.0
+//        button.layer.borderColor = UIColor.red.cgColor
+//        button.layer.cornerRadius = 5
+//        button.layer.masksToBounds = true
         button.addTarget(self, action: #selector(onSart), for: .touchUpInside)
         return button
     }()
@@ -53,19 +70,35 @@ class UserTableViewCell: UITableViewCell {
     }
     
     private func setupView() {
+        contentView.addSubview(backgroundImage)
+        contentView.addSubview(userImage)
         contentView.addSubview(lblUsername)
         contentView.addSubview(btnStart)
         contentView.addSubview(activityIndicator)
+        contentView.layer.backgroundColor = UIColor(hex: "F5F8FA").cgColor
         activityIndicator.isHidden = true
-        lblUsername.snp.makeConstraints { make in
+        backgroundImage.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().inset(10)
+            make.top.equalToSuperview().offset(10)
+            make.bottom.equalToSuperview()
+        }
+        userImage.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(20)
-            make.top.bottom.equalToSuperview()
+            make.top.bottom.equalTo(backgroundImage)
+            make.width.equalTo(45)
+            make.height.equalTo(50)
+        }
+        lblUsername.snp.makeConstraints { make in
+            make.leading.equalTo(userImage.snp.trailing).offset(20)
+            make.top.bottom.equalTo(backgroundImage)
             make.trailing.equalTo(btnStart.snp.leading).inset(10)
         }
         btnStart.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview().inset(2)
+            make.top.bottom.equalTo(backgroundImage).inset(2)
             make.trailing.equalToSuperview().inset(20)
-            make.width.equalTo(70)
+            make.width.equalTo(50)
+            make.height.equalTo(50)
         }
         activityIndicator.snp.makeConstraints { make in
             make.center.equalTo(btnStart)
@@ -81,7 +114,10 @@ class UserTableViewCell: UITableViewCell {
     }
     func setData(user: User) {
         self.user = user
-        lblUsername.text = user.username
+        lblUsername.text = user.username?.uppercased()
+        if let avatar = user.avatarImage {
+            userImage.image = UIImage(named: avatar)
+        }
         btnStart.isHidden = false
     }
 }
