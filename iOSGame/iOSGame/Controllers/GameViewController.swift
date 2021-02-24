b54418849062e0762402795712b8473b5f35b1a3
Deniz Adil <xdenizadil@gmail.com>
2021-02-24 19:16:35 +0100

    • Exit button created with alert implemented

--- iOSGame/iOSGame/Controllers/GameViewController.swift
+++ iOSGame/iOSGame/Controllers/GameViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet weak var lblGameStatus: UILabel!
+    
     var game: Game?
     
     override func viewDidLoad() {
@@ -18,7 +19,18 @@ class GameViewController: UIViewController {
        
     }
     
-
+    @IBAction func onClose(_ sender: UIButton) {
+        let alert = UIAlertController(title: nil, message: "Are you sure you want to exit?", preferredStyle: .alert)
+        let exit = UIAlertAction(title: "Exit", style: .destructive) { [weak self] _ in
+            // we need to update the oher player
+            self?.dismiss(animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(exit)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
  
 
 }
